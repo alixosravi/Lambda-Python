6a046c96b23bde9f89465000e7be1a7375d433f4
Ali Xosravi <ali.xosravi.1987@gmail.com>
2023-10-30 09:37:39 -0700

    Added lambda-py to my project

---
+++ lambda.py
@@ -0,0 +1,9 @@
+my_list = ["aaa", "abbaa", "a", "aa", "aaaaa"]
+# map
+# filter
+# sorted
+
+new_list1 = map(lambda x: x * 3, my_list)
+new_list2 = filter(lambda x: x > 5, my_list)
+
+new_list3 = sorted(my_list, key=lambda x: len(x))
